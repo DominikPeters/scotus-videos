f67bec987518c9a4100e99028aa83a53f3932289
Dominik Peters <mail@dominik-peters.de>
2024-01-18 09:00:44 +0000

    script updates

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y sox ffmpeg
+sudo apt-get install -y sox libsox-fmt-mp3 ffmpeg
 pip install sox b2sdk pillow google-api-python-client google-auth-oauthlib
 npm install puppeteer
+mkdir json frames thumbnails videos
